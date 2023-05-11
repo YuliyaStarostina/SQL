d4c9fb13cde44588267ed5a657a6d048bfbd74c6
Starostina <pankovays@mail.ru>
2023-05-11 11:37:15 +0700

    home_6

---
+++ home_6.1.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE second_times(counts INT)
+BEGIN
+	CASE
+		WHEN counts < 60 THEN
+			SELECT CONCAT(counts, ' ', 'seconds') AS Result;
+        WHEN counts >= 60 AND counts < 3600 THEN
+			SELECT CONCAT_WS(' ', counts DIV 60, 'minutes', MOD(counts, 60), 'seconds') AS Result;
+        WHEN counts >= 3600 AND counts < 86400 THEN
+			SELECT CONCAT_WS(' ', counts DIV 3600, 'hours', MOD(counts, 3600) DIV 60, 'minutes', MOD(MOD(counts, 3600),60), 'seconds') AS Result;
+        ELSE
+			SELECT CONCAT_WS(' ', counts DIV 86400, 'days', MOD(counts, 86400) DIV 3600, 'hours', MOD(MOD(counts, 86400),3600) DIV 60, 'minutes',
+                             MOD(MOD(MOD(counts, 86400),3600),60), 'seconds') AS Result;
+    END CASE;
+END//
+
+CALL second_times(12378);  
